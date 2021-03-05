e3e46543b58f753b21be8dcdbdd37ad112d01e73
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2021-03-05 19:59:00 +0900

    final upload react router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink, useParams} from 'react-router-dom';
 
 function Home() {
   return (
@@ -13,26 +13,48 @@ function Home() {
   )
 }
 
+var contents = [
+  {id: 1, title: 'HTML', description: 'HTML is ...'},
+  {id: 2, title: 'JS', description: 'JS is ...'},
+  {id: 3, title: 'React', description: 'React is ...'},
+]
+
+function Topic() {
+  var params = useParams();
+  var topic_id = params.topic_id;
+  var selected_topic = {
+    title: 'Sorry',
+    description: 'Not Found',
+  }
+  for(var i = 0; i < contents.length; i ++) {
+    if(contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i];
+      break;
+    }
+  }
+  console.log(params)
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+        {selected_topic.description}
+    </div>
+  );
+}
+
 function Topics() {
+  var lis = [];
+  for(var i = 0; i<contents.length; i ++) {
+    lis.push(<li key={contents[i].id}><NavLink to={'/topics/' + contents[i].id}>{contents[i].title}</NavLink></li>)
+  }
   return (
     <div>
       <h2>Topics</h2>
       <ul>
-        <li><NavLink to='/topics/1'>HTML</NavLink></li>
-        <li><NavLink to='/topics/2'>JS</NavLink></li>
-        <li><NavLink to='/topics/3'>React</NavLink></li>
+        {lis}
       </ul>
-      <Switch>
-        <Route path='/topics/1'>
-          HTML is ...
-        </Route>
-        <Route path='/topics/2'>
-          JS is ...
-        </Route>
-        <Route path='/topics/3'>
-          React is ...
-        </Route>
-      </Switch>
+      <Route path='/topics/:topic_id'>
+        <Topic></Topic>
+      </Route>
     </div>
   )
 }
